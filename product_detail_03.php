63a72007354a314739b6bdb0fe5b1ec92ff2b31f
wu112390 <39981700+wu112390@users.noreply.github.com>
2018-08-23 10:30:59 +0800

    s_0823
    第一次更新

---
+++ product_detail_03.php
@@ -0,0 +1,54 @@
+<section id="product_detail_03"> 
+    <div class="index_conten">
+        <div class="product_detail_03_sec1 flex">
+            <div class="product_detail_03_sec1_left">
+                <div class="product_detail_03_sec1_txt">
+                    <h2 class="product_detail_03_h2">From<br>Copenhagen<br><br></h2>
+                    <p class="product_detail_03_p">一打開家門，都希望映入眼簾的是舒適清爽的環境，感受到的是放鬆無壓的氛圍，讓你一走進就感受到溫暖寧靜。觀察過許多不同類型的裝修風格，發現最能恆久，百看不厭，也最能讓人感受療癒，就是利用中性色調，營造出自然柔和的感受。
+                    <br>
+                    <br>
+                    所謂的中性色調，就如同象牙白、米色、灰、駝色和咖啡等色系，運用在家居裝飾上，能營造出小空間的寬闊感，也能呼應自然光，襯托出空間寧靜愜意的感受。</p>
+                    </div>
+            </div>
+            <div class="product_detail_03_sec1_right"></div>
+        </div>
+        <div class="product_detail_03_sec2 flex">
+            <div class="product_detail_03_sec2_left"></div> 
+            <div class="product_detail_03_sec2_right">
+                <div class="product_detail_03_sec2_right_box"></div>
+            </div>
+        </div>
+        <div class="product_detail_03_sec3 flex">
+            <div class="product_detail_03_sec3_left">
+                <div class="product_detail_03_sec3_txt">
+                    <h2 class="product_detail_03_h2">Comes<br>In<br>Various<br>Sizes<br><br></h2>
+                    <p class="product_detail_03_p">寬度: 150cm 高度: 85cm 深度: 150cm
+                    <br>淨重: 2.45kg
+                    <br>材質密度: 0.93cbm
+                    <br>英格蘭短羊毛 80%
+                    <br><br>
+                    </p>
+                </div>
+                <div class="product_detail_03_sec3_left_box"></div>
+            </div>
+            <div class="product_detail_03_sec3_right">
+                
+            <div class="product_detail_03_sec3_right_box"></div>
+            </div>
+        </div>
+        <div class="product_detail_03_sec4 flex">
+            <div class="product_detail_03_sec4_left">
+            </div>
+            <div class="product_detail_03_sec4_right">
+                <div class="product_detail_03_sec4_right_txt">
+                    <h3 class="product_detail_03_h3">From<br>Copenhagen<br><br></h3>
+                    <p class="product_detail_03_p">一打開家門，都希望映入眼簾的是舒適清爽的環境，感受到的是放鬆無壓的氛圍，讓你一走進就感受到溫暖寧靜。觀察過許多不同類型的裝修風格，發現最能恆久，百看不厭，也最能讓人感受療癒，就是利用中性色調，營造出自然柔和的感受。</p>
+                </div>
+            </div>
+
+        </div>
+        <div class="product_detail_03_sec5">
+            <figure></figure>
+        </div>
+    </div>   
+</section>
