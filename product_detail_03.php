56a17a44d10e60e56b1fe8e2910104651543bb54
yakuo <prsal2253@gmail.com>
2018-08-27 15:15:32 +0800

    Y_0827

--- product_detail_03.php
+++ product_detail_03.php
@@ -1,54 +1,55 @@
 <section id="product_detail_03"> 
-    <div class="index_conten">
-        <div class="product_detail_03_sec1 flex">
-            <div class="product_detail_03_sec1_left">
-                <div class="product_detail_03_sec1_txt">
-                    <h2 class="product_detail_03_h2">From<br>Copenhagen<br><br></h2>
-                    <p class="product_detail_03_p">一打開家門，都希望映入眼簾的是舒適清爽的環境，感受到的是放鬆無壓的氛圍，讓你一走進就感受到溫暖寧靜。觀察過許多不同類型的裝修風格，發現最能恆久，百看不厭，也最能讓人感受療癒，就是利用中性色調，營造出自然柔和的感受。
-                    <br>
-                    <br>
-                    所謂的中性色調，就如同象牙白、米色、灰、駝色和咖啡等色系，運用在家居裝飾上，能營造出小空間的寬闊感，也能呼應自然光，襯托出空間寧靜愜意的感受。</p>
+        <div class="index_conten product_detail_03">
+            <div class="product_detail_03_sec1 flex">
+                <div class="product_detail_03_sec1_left flex">
+                    <div class="product_detail_03_sec1_left01">
+                        <div class="product_detail_03_sec1_txt">
+                            <h2 class="product_detail_03_h2" style="color: #fff;letter-spacing: 0.05em">Tonella<br>Arm Chair</h2>
+                        </div>
                     </div>
-            </div>
-            <div class="product_detail_03_sec1_right"></div>
-        </div>
-        <div class="product_detail_03_sec2 flex">
-            <div class="product_detail_03_sec2_left"></div> 
-            <div class="product_detail_03_sec2_right">
-                <div class="product_detail_03_sec2_right_box"></div>
-            </div>
-        </div>
-        <div class="product_detail_03_sec3 flex">
-            <div class="product_detail_03_sec3_left">
-                <div class="product_detail_03_sec3_txt">
-                    <h2 class="product_detail_03_h2">Comes<br>In<br>Various<br>Sizes<br><br></h2>
-                    <p class="product_detail_03_p">寬度: 150cm 高度: 85cm 深度: 150cm
-                    <br>淨重: 2.45kg
-                    <br>材質密度: 0.93cbm
-                    <br>英格蘭短羊毛 80%
-                    <br><br>
-                    </p>
+                    <div class="product_detail_03_sec1_left02">
+                        <div class="product_detail_03_sec1_txt">
+                            <h2 class="product_detail_03_h2">From<br>Copenhagen</h2>
+                            <h3 class="product_detail_03_h3">一打開家門，都希望映入眼簾的是舒適清爽的環境，感受到的是放鬆無壓的氛圍，讓你一走進就感受到溫暖寧靜。觀察過許多不同類型的裝修風格，發現最能恆久，百看不厭，也最能讓人感受療癒，就是利用中性色調，營造出自然柔和的感受。所謂的中性色調，就如同象牙白、米色、灰、駝色和咖啡等色系，運用在家居裝飾上，能營造出小空間的寬闊感，也能呼應自然光，襯托出空間寧靜愜意的感受。</h3>
+                        </div>
+                    </div>
+                    <div class="product_detail_03_sec1_left03"></div>
                 </div>
-                <div class="product_detail_03_sec3_left_box"></div>
-            </div>
-            <div class="product_detail_03_sec3_right">
-                
-            <div class="product_detail_03_sec3_right_box"></div>
-            </div>
-        </div>
-        <div class="product_detail_03_sec4 flex">
-            <div class="product_detail_03_sec4_left">
-            </div>
-            <div class="product_detail_03_sec4_right">
-                <div class="product_detail_03_sec4_right_txt">
-                    <h3 class="product_detail_03_h3">From<br>Copenhagen<br><br></h3>
-                    <p class="product_detail_03_p">一打開家門，都希望映入眼簾的是舒適清爽的環境，感受到的是放鬆無壓的氛圍，讓你一走進就感受到溫暖寧靜。觀察過許多不同類型的裝修風格，發現最能恆久，百看不厭，也最能讓人感受療癒，就是利用中性色調，營造出自然柔和的感受。</p>
+                <div class="product_detail_03_sec1_right flex">
+                    <div class="product_detail_03_sec1_right01"></div>
+                    <div class="product_detail_03_sec1_right02"></div>
+                    <div class="product_detail_03_sec1_right03">
+                        <div class="product_detail_03_sec1_txt">
+                            <h2 class="product_detail_03_h2" style="color: #fff">Comes<br>In<br>Various<br>Sizes</h2>
+                        </div>
+                    </div>
+                    <div class="product_detail_03_sec1_right04">
+                        <div class="product_detail_03_sec1_txt">
+                            <h3 class="product_detail_03_h3" style="font-size: 20px;font-weight: 500; font-family:'SourceHanSerifTC-Bold';letter-spacing: 0.05em;margin-top:0">商品尺寸</h3>
+                            <h3 class="product_detail_03_h3">
+                                寬度: 150cm 高度: 85cm 深度: 150cm <br>
+                                淨重: 2.45kg <br>
+                                材質密度: 0.93cbm <br> 
+                                英格蘭短羊毛 80% <br>
+                            </h3>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-        </div>
-        <div class="product_detail_03_sec5">
-            <figure></figure>
-        </div>
-    </div>   
-</section>
+            <div class="product_detail_03_sec2">
+                <div class="product_detail_03_sec2_01">
+                    <div class="product_detail_03_sec2_01_bg">
+                        <div class="product_detail_03_sec2_01_bg_box">
+                            <div class="product_detail_03_sec1_txt">
+                                <h2 class="product_detail_03_h2">The Dream Chair<br>That Lits Up<br>Your Home</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product_detail_03_sec2_01_top">
+                        <div class="product_detail_03_sec2_01_top_box"></div>
+                    </div>
+                </div>
+                <div class="product_detail_03_sec2_03"></div>
+            </div>
+        </div>   
+    </section>
