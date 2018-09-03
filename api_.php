d8e5d8ddc5ccca6fdec3d137342ff8e564017f15
orangejan <39645580+orangejan@users.noreply.github.com>
2018-09-03 14:04:30 +0800

    購物車搞定

---
+++ api_.php
@@ -0,0 +1,36 @@
+<?php
+require __DIR__. '/__db_connect.php';
+
+print_r($_POST);
+
+    $sql = "UPDATE `members` SET `name`=?, `gender`=?, `mobile`=?,`address_city`=?,
+ `address_side`=?, `address_post`=?, `address`=? , `year`=?, 
+ `month`=?,`day`=? WHERE `member_sid`=?";
+//最後一格後面不能有逗號！
+    $stmt = $mysqli->prepare($sql);
+//            $stmt去接mysqli那個值
+    $stmt->bind_param('ssssssssssi',
+        $_POST['name'],
+        $_POST['gender'],
+        $_POST['mobile'],
+        $_POST['address_city'],
+
+        $_POST['address_side'][0],
+        $_POST['address_post'],
+        $_POST['address'],
+        $_POST['year'],
+
+        $_POST['month'],
+        $_POST['day'],
+        $_SESSION['user']['member_sid']
+//最後一格後面不能有逗號！
+    );
+
+    $stmt->execute();
+    $af = $stmt->affected_rows;
+    print_r($af);
+    print_r($stmt);
+$stmt->close();
+
+
+
