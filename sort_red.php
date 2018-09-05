f4c108e47f533431eb2b90872107176e22aea379
wu112390 <39981700+wu112390@users.noreply.github.com>
2018-09-05 18:42:04 +0800

    s

--- sort_red.php
+++ sort_red.php
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>The Palette</title>
-    <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/sort_red.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css" />
+<?php include 'page_item/head.php';?>
 </head>
 <body>
+    <!-- top -->
+<div class="index_top">
+    <?php include 'page_item/header.php';?> 
+</div>
     <?php include 'sort_red01.php';?>
     <?php include 'sort_red02.php';?>
     <?php include 'sort_red03.php';?>
