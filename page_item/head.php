abc551b25cd447687b06487183e201f163109f9b
weiweilee826 <42629336+weiweilee826@users.noreply.github.com>
2018-09-06 14:08:00 +0800

    w

--- page_item/head.php
+++ page_item/head.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="css/style.css?123">
     <!-- 會員中心 --><link rel="stylesheet" href="css/member.css?123">
     <!-- 商品詳細 --><link rel="stylesheet" href="css/product_detail.css?123">
-    <!-- 元件css --><link rel="stylesheet" href="css/pegeitem.css?123">
+    <!-- 元件css --><link rel="stylesheet" href="css/pageitem.css?123">
     <!-- 館別商品列表 --><link rel="stylesheet" href="css/sort_red.css?123">
     <link rel="stylesheet" href="css/flatpickr.min.css?123">
     <!-- ＪＱ --><script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
