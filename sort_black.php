ef19314d65bee44658fb26fd0dd60162b3e75849
weiweilee826 <42629336+weiweilee826@users.noreply.github.com>
2018-09-07 17:11:07 +0800

    w

---
+++ sort_black.php
@@ -0,0 +1,233 @@
+<?php include 'page_item/head.php';?>
+</head>
+<body>
+    <!-- top -->
+<div class="index_top">
+    <?php include 'page_item/header.php';?> 
+</div>
+    
+<div id="sort_black01">
+<div id="product_detail_01"> 
+    <div class="index_top">
+        <header><h1 class="product_detail_h1">palette</h1></header>
+    </div>
+        <section>
+            <div class="index_main sort_black01">
+                <div class="sort_black01_bg">
+                    <div class="sort_black01_bg_l"></div>
+                    <div class="sort_black01_bg_r"></div>
+                </div>
+                <div class="sort_black01_top">
+                    <figure>
+                        <div class="sort_black01_top_topic">
+                            <h2>Black<br>White<br>Grey</h2><br><br>
+                            <h3>黑<br>白<br>灰</h3>
+                        </div>
+                        <div class="sort_black01_top_box1"></div>
+                        <div class="sort_black01_top_box2"></div>
+                    </figure>
+                </div>
+            </div>
+        </section>    
+</div>
+<!-- 黑色介紹 -->
+<div id="" class="transition changebg"> 
+    <section>
+        <div class="index_conten_flex sort_black02">
+            <div class="sort_black02_intro">
+                <div class="sort_black02_intro_top flex">
+                    <div class="sort_black02_intro_l flex">
+                        <h2 class="sort_black02_h2">Black</h2>
+                        <div class="sort_black02_intro_l_txt">
+                            <h3>簡潔的黑 高雅的黑 經典的黑</h3><br><br>
+                            <p>對於黑，達文西說：「黑不是顏色。」話雖如此，黑仍是他調色盤上的一抹顏料，而且他還很常使用──用來畫背景。他有一幅畫成，然而她身後卻是扎扎實實、不透明的黑。她還帶了一串黑項鍊。而在達文西的畫作中，耶穌幽幽於我們眼前浮現，似乎是從死者的世界望了過來，一雙棕色的眼睛如蒙薄翳、彷彿目不視物，身後則是煤炭或煙灰的純黑（圖1）。這麼說來，在達文西認為，即便黑不是顏色，仍十分適合作為背景，烘托出其他色彩。</p>
+                        </div>                            
+                        
+                    </div>
+                    <div class="sort_black02_intro_r"></div>
+                </div>
+                <div class="sort_black02_intro_bg">
+                    <div class="sort_black02_intro_box changebox"></div>
+                </div>
+            </div>
+            <div class="sort_black02_rec">
+                <h5>推薦商品</h5>
+                <figure>
+                    <img src="images/icon/next_w.svg" alt="">
+                </figure>
+            </div>
+            <div class="sort_black02_rec_product flex">
+                <a href="/" class="sort_black02_rec_product_s">
+                    <div class="sort_black02_rec_product01 sort_black02_rec_product_scale transition"></div>
+                    <h6>Stacked System</h6>
+                </a>
+                <a href="/" class="sort_black02_rec_product_s">
+                    <div class="sort_black02_rec_product02 sort_black02_rec_product_scale transition"></div>
+                    <h6>Oscar Sectional Sofa</h6>
+                </a>
+                <a href="/" class="sort_black02_rec_product_s">
+                    <div class="sort_black02_rec_product03 sort_black02_rec_product_scale transition"></div>
+                    <h6>Fringe Pouf</h6>
+                </a>
+                <a href="/" class="sort_black02_rec_product_s">
+                    <div class="sort_black02_rec_product04 sort_black02_rec_product_scale transition"></div>
+                    <h6>Fringe Pouf</h6>
+                </a>
+            </div>
+        </div>
+    </section>
+</div>
+
+<!-- 白色介紹 -->
+<div id="" class="transition changebg"> 
+    <section>
+        <div class="index_conten_flex sort_black03">
+            <div class="sort_black03_intro">
+                <div class="sort_black03_intro_top flex">
+
+                    <div class="sort_black03_intro_r"></div>
+
+                    <div class="sort_black03_intro_l flex">
+                        
+                        <div class="sort_black03_intro_l_txt">
+                            <h3>熱情的橘 沉穩的橘 撩人的橘</h3><br><br>
+                            <p>改編自 Jenny Han 的同名青春愛情小說，內容講述亞裔女孩 Lara Jean 這名默默無名的女孩，有著溫柔、聰穎的性格，卻十分內向。她的心思細膩，對那些「愛過的男孩們」卻不曾訴說心中的情感，這是她永藏心底的秘密。當熾熱的感情無處渲洩時，Lara Jean 會寫下「情書」藏在帽盒裡。</p>
+                        </div>
+                        <h2 class="sort_black03_h2">White</h2>                                 
+                    </div>    
+                </div>
+                <div class="sort_black03_intro_bg">
+                    <div class="sort_black03_intro_box changebox"></div>
+                </div>
+            </div>
+
+            <div class="sort_black03_rec">
+                <h5>推薦商品</h5>
+                <figure>
+                    <img src="images/icon/next.svg" alt="">
+                </figure>
+            </div>
+            <div class="sort_black03_rec_product flex">
+                <a href="/" class="sort_black03_rec_product_s">
+                    <div class="sort_black03_rec_product01 sort_black03_rec_product_scale transition"></div>
+                    <h6>Stacked System</h6>
+                </a>
+                <a href="/" class="sort_black03_rec_product_s">
+                    <div class="sort_black03_rec_product02 sort_black03_rec_product_scale transition"></div>
+                    <h6>Stacked System</h6>
+                </a>
+                <a href="/" class="sort_black03_rec_product_s">
+                    <div class="sort_black03_rec_product03 sort_black03_rec_product_scale transition"></div>
+                    <h6>Stacked System</h6>
+                </a>
+                <a href="/" class="sort_black03_rec_product_s">
+                    <div class="sort_black03_rec_product04 sort_black03_rec_product_scale transition"></div>
+                    <h6>Fringe Pouf</h6>
+                </a>
+            </div>
+        </div>
+    </section>
+</div>
+
+<!-- 灰色介紹 -->
+<div id="" class="transition changebg"> 
+    <section>
+        <div class="index_conten_flex sort_black04">
+            <div class="sort_black04_intro">
+                <div class="sort_black04_intro_top flex">
+                    <div class="sort_black04_intro_l flex">
+                        <h2 class="sort_black04_h2">Grey</h2>
+                        <div class="sort_black04_intro_l_txt">
+                            <h3>熱情的粉 沉穩的粉 撩人的粉</h3><br><br>
+                            <p>改編自 Jenny Han 的同名青春愛情小說，內容講述亞裔女孩 Lara Jean 這名默默無名的女孩，有著溫柔、聰穎的性格，卻十分內向。她的心思細膩，對那些「愛過的男孩們」卻不曾訴說心中的情感，這是她永藏心底的秘密。</p>
+                        </div>                            
+                        
+                    </div>
+                    <div class="sort_black04_intro_r"></div>
+                </div>
+                <div class="sort_black04_intro_bg">
+                    <div class="sort_black04_intro_box changebox"></div>
+                </div>
+            </div>
+            <div class="sort_black04_rec">
+                <h5>推薦商品</h5>
+                <figure>
+                    <img src="images/icon/next_w.svg" alt="">
+                </figure>
+            </div>
+            <div class="sort_black04_rec_product flex">
+                <a href="/" class="sort_black04_rec_product_s">
+                    <div class="sort_black04_rec_product01 sort_black04_rec_product_scale transition"></div>
+                    <h6>Stacked System</h6>
+                </a>
+                <a href="/" class="sort_black04_rec_product_l">
+                    <div class="sort_black04_rec_product02 sort_black04_rec_product_scale transition"></div>
+                    <h6>Oscar Sectional Sofa</h6>
+                </a>
+                <a href="/" class="sort_black04_rec_product_s">
+                    <div class="sort_black04_rec_product03 sort_black04_rec_product_scale transition"></div>
+                    <h6>Fringe Pouf</h6>
+                </a>
+            </div>
+        </div>
+    </section>
+</div>
+
+<?php include 'page_item/footer.php';?>  
+    <script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
+
+<script>
+$(window).scroll(function() {
+    var scrolltop = $(this).scrollTop();
+    if (scrolltop>300 && scrolltop<2000){
+        $(".changebg").css('background-color','#000');
+    }else if(scrolltop>2000 && scrolltop<3400){
+        $('.changebg').css('background-color','#fff');
+    }else if(scrolltop>3400){
+        $('.changebg').css('background-color','rgb(190,190,190)');
+    }
+});
+
+$(window).scroll(function() {
+    var scrolltop = $(this).scrollTop();
+    if (scrolltop>300 && scrolltop<2000){
+        $(".changebox").css('background-color','#000');
+    }else if(scrolltop>2000 && scrolltop<3400){
+        $('.changebox').css('background-color','#fff');
+    }else if(scrolltop>3400){
+        $('.changebox').css('background-color','rgb(190,190,190)');
+    }
+});
+
+// filter
+        // 選擇顏色
+        $(".filter_color_box").click(function () {
+            $(this).toggleClass("toggle_color");
+        });
+
+        $(".filter_filter").click(function () {
+            $(".filter_inner").toggleClass("filter_open")
+        })
+        //選品項
+        $(".filter_items").click(function(){
+            $(this).toggleClass("item_choose");
+        })
+
+        //顯示range的值
+			function change() {
+  				var value = document.getElementById('range').value ;
+  				document.getElementById('value').innerHTML = value;
+                console.log("#value");
+            }
+            function change2() {
+  				var value = document.getElementById('range2').value ;
+  				document.getElementById('value2').innerHTML = value;
+                console.log("#value");
+            }
+</script>
+</body>
+</html>
