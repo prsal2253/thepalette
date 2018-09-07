beebaccdfda2c4c3a3fa733c640b22e052f17867
orangejan <39645580+orangejan@users.noreply.github.com>
2018-09-07 20:27:03 +0800

    老師修改

--- sort_red_api.php
+++ sort_red_api.php
@@ -1,70 +1,139 @@
 <?php
-require __DIR__ . '/../__db_connect.php';
 
-if(!empty($_SESSION['cart'])) {
-    $keys = array_keys($_SESSION['cart']);
-//字面上意思是拿到$_SESSION['cart']所有的key
-    $sql = sprintf("SELECT * FROM `products_list` WHERE `product_sid` IN (%s)", implode(',', $keys));
-    //IN (這邊要塞sid逗號隔開)
-//黏著符號js叫做join()
-//php叫做implode
-//SELECT * FROM `products_list` WHERE `product_sid` IN (1,2)
-    $rs = $mysqli->query($sql);
+//require __DIR__ . '/__db_connect.php';
+$mysqli = new mysqli('localhost', 'orange', '0987', 'the palette');
 
-    $data = [];
+$mysqli->query("SET NAMES utf8");
 
-    while ($r = $rs->fetch_assoc()) {
-        $r['qty'] = $_SESSION['cart'][$r['product_sid']];
 
-        $data[$r['product_sid']] = $r;
-    }
+$pageName = 'product_list_red';
 
+$build_query = [];
+
+# 商品資料 begin>
+$per_page = 16; //一頁有幾筆
+$page = isset($_GET['page']) ? intval($_GET['page']) : 1; //用戶要看第幾頁
+//intval取整數
+$page1 = $page + 1;
+$page2 = $page - 1;
+
+
+$color = isset($_GET['color']) ? $_GET['color'] : 0; //顏色
+$items = isset($_GET['items']) ? $_GET['items'] : 0;//種類
+$long = isset($_GET['long']) ? intval($_GET['long']) : 0;//寬度
+$high = isset($_GET['high']) ? intval($_GET['high']) : 0;//高度
+$price = isset($_GET['price']) ? intval($_GET['price']) : 0; //時間價格
+
+$where = " WHERE 1  ";
+
+if(!empty($color)){
+    $c = explode(',', $color);
+    $color_condition = ' AND (product_color_sid='. implode(' OR product_color_sid=', $c) . ')';
+    // 2 OR product_color_sid=3
+    $where .= $color_condition;
+}
+
+if(!empty($items)){
+    $i = explode(',', $items);
+    $items_condition = ' AND (category_sid='. implode(' OR category_sid=', $i) . ')';
+    $where .= $items_condition;
+}
+
+if ($long == 50) {
+    $where .= " AND `size_sid_w`=1";
+    $build_query['long'] = $long;
+} elseif ($long == 100) {
+    $where .= " AND `size_sid_w`=2";
+    $build_query['long'] = $long;
+} elseif ($long == 150) {
+    $where .= " AND `size_sid_w`=3";
+    $build_query['long'] = $long;
+}
+
+if ($high == 50) {
+    $where .= " AND `size_sid_h`=1";
+    $build_query['high'] = $high;
+} elseif ($high == 100) {
+    $where .= " AND `size_sid_h`=2";
+    $build_query['high'] = $high;
+} elseif ($high == 150) {
+    $where .= " AND `size_sid_h`=3";
+    $build_query['high'] = $high;
 }
 
 
+if ($price == 1) {
+    $where .= " ORDER BY `price` ASC ";
+    $build_query['price'] = $price;
+} elseif ($price == 2) {
+    $where .= "  ORDER BY `price` DESC  ";
+    $build_query['price'] = $price;
+}elseif ($price == 3) {
+    $where .= "  ORDER BY `publish_date` ASC  ";
+    $build_query['price'] = $price;
+}elseif ($price == 4) {
+    $where .= "  ORDER BY `publish_date` DESC  ";
+    $build_query['price'] = $price;
+}
 
 
-if(!empty($_SESSION['cart'])): ?>
-    <?php
-    $total = 0;
-    foreach($keys as $k):
-        $r = $data[$k]; // 整筆資料(包含 qty)
-        $total += $r['price'] * $r['qty'];
-        ?>
-        <div class="order_listbox" data-sid="<?= $k ?>">
-            <figure class="description_20"><a href="#"><img src="images/<?= $r['img'] ?>.png" alt="<?= $r['product_name'] ?>"></a></figure>
-            <div class="description_70">
-                <a href="#" class="product_name"><?= $r['product_name'] ?></a>
-                <p>Ｘ<?= $r['qty'] ?></p>
-                <p>$<?= $r['price'] ?></p>
+$total_sql = "SELECT COUNT(1) FROM `products_list` $where";
+$total_rows = $mysqli->query($total_sql)->fetch_row()[0]; //這邊拿到的是總筆數
+$total_pages = ceil($total_rows / $per_page);
+
+
+$product_sql = sprintf("SELECT * FROM  `products_list` $where LIMIT %s, %s ", ($page - 1) * $per_page, $per_page);
+
+//echo $product_sql; exit;
+//這裡會拿到sql的字串
+$product_rs = $mysqli->query($product_sql);
+
+
+?>
+
+    <div class="sort_red05_row flex">
+        <?php while ($r = $product_rs->fetch_assoc()): ?>
+            <div name="product" class="sort_red05_box_s product_sid_data" data-sid="<?= $r['product_sid'] ?>">
+                <img src="images/<?= $r['img'] ?>.png" alt="<?= $r['product_name'] ?>">
+                <div class="product_mask transition">
+                    <div class="product_favorate transition"></div>
+                    <div class="product_name_nd_btn">
+                        <div class="product_name">
+                            <h3 class="product_name_h3"><a href="#"
+                                                           id="product_name"><?= $r['product_name'] ?></a></h3>
+                        </div>
+                        <div class="product_btn"></div>
+                        <a href="product_quicklook.php?id=<?= $r['product_sid'] ?>"
+                           class="palette_btn quick_look_palette_btn quick"
+                           data-fancybox
+                           data-options='{"type" : "iframe", "iframe" : {"preload" : false, "css" : {"width" : "1000px","height" :
+                                   "70vh"}}}'>快速查看</a>
+                    </div>
+                </div>
             </div>
-            <div class="description_10"><div class="icon_garbage"></div></div>
-        </div>
-    <?php endforeach; ?>
-
-    <div class="check_outbox"><a class="check_out" href="/thepalette/thepalette/shoppingcar_01.php">CHECK OUT</a></div>
-
-<?php else: ?><!-- 購物車沒有商品時的狀態 -->
-    <div class="order_listbox carts_none">
-        <h3>購物車目前沒有任何商品</h3>
+        <?php endwhile; ?>
     </div>
-<?php endif ?>
-
-
-<script>
-    $(".car_icon").click(function () {
-        $(this).parents().find(".car_iconhover").toggleClass("menu_active");
-        //即時更新
-        $.get('header_api.php', function (data) {
-            $('.car_iconhover').html(data);
-        }, 'text');
-    });
-
-
-
-    var changeSmallCart = function () {
-        $.get('./page_item/header_api.php', function (data) {
-            $('.car_iconhover').html(data);
-        }, 'text');
-    };
-</script>
+    <!-- 頁碼 -->
+    <div class="sort_red05_page">
+        <ul>
+            <a <?= $page == 1 ? "style='display: none'" : "href='?page=" . $page2 . "&" . http_build_query($build_query) . "'" ?>>
+                <!--                           接字串的方式 $page2是變數 前後加上. -->
+                <li class="page_prev">
+                    <figure></figure>
+                    PREV
+                </li>
+            </a>
+            <?php for ($i = 1; $i <= $total_pages; $i++): ?>
+                <li class="page p<?= $i == $page ? 'active' : '' ?>">
+                    <a <?= $page == $i ? '' : "href='?page=" . $i . "&" . http_build_query($build_query) . "'" ?>>
+                        <p><?= $i ?></p></a>
+                </li>
+            <?php endfor ?>
+            <a <?= $page == $total_pages ? "style='display: none'" : "href='?page=" . $page1 . "&" . http_build_query($build_query) . "'" ?>>
+                <li class="page_next">
+                    <figure></figure>
+                    NEXT
+                </li>
+            </a>
+        </ul>
+    </div>
